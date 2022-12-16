b0942f440dc1139d7b835968c0f54cc50550e0a1
Nate Ensign <dixienate@gmail.com>
2022-12-15 17:20:32 -0700

    complete through day 3 instruction

---
+++ server/controllers/auth.js
@@ -0,0 +1,12 @@
+
+
+
+module.exports = {
+    register: (req, res) => {
+        console.log('register')
+    },
+
+    login: (req, res) => {
+        console.log('login')
+    },
+}
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,62 +1,64 @@
-import {useState, useContext} from 'react'
-import axios from 'axios'
-import AuthContext from '../store/authContext'
- 
+import { useState, useContext } from "react";
+import axios from "axios";
+import AuthContext from "../store/authContext";
+
 const Auth = () => {
-   const [username, setUsername] = useState('')
-   const [password, setPassword] = useState('')
-   const [register, setRegister] = useState(true)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(true);
 
-   const authCtx = useContext(AuthContext)
- 
-   const submitHandler = e => {
-       e.preventDefault()
+  const authCtx = useContext(AuthContext);
 
-       const body = {
-        username,
-        password
-       }
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-       const url = 'https://socialmtn.devmountain.com'
+    const body = {
+      username,
+      password,
+    };
 
-       axios.post(register ? `${url}/register` : `${url}/login`, body)
-       .then((res) => {
-        console.log('AUTH RUN', res.data)
-        authCtx.login(res.data.token, res.data.exp, res.data.userId)
-       })
-       .catch(err => {
-        setPassword('')
-        setUsername('')
-        console.log('error')
-       })
+    const url = "https://socialmtn.devmountain.com";
 
-       console.log('submitHandler called')
-   }
- 
-   return (
-       <main>
-           <h1>Welcome!</h1>
-           <form className='form auth-form' onSubmit={submitHandler}>
-               <input
-                   type='text'
-                   placeholder='username'
-                   value={username}
-                   onChange={e => setUsername(e.target.value)}
-                   className='form-input'
-                    />
-               <input
-                   type='text'
-                   placeholder='password'
-                   value={password}
-                   onChange= {e => setPassword(e.target.value)}
-                   className='form-input'/>
-               <button className='form-btn'>
-                   {register ? 'Sign Up' : 'Login'}
-               </button>
-           </form>
-           <button className='form-btn'>Need to {register ? 'Login' : 'Sign Up'}?</button>
-       </main>
-   )
-}
- 
-export default Auth
+    axios
+      .post(register ? `${url}/register` : `${url}/login`, body)
+      .then((res) => {
+        console.log("AUTH RUN", res.data);
+        authCtx.login(res.data.token, res.data.exp, res.data.userId);
+      })
+      .catch((err) => {
+        setPassword("");
+        setUsername("");
+        console.log("error");
+      });
+
+    console.log("submitHandler called");
+  };
+
+  return (
+    <main>
+      <h1>Welcome!</h1>
+      <form className="form auth-form" onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="form-input"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-input"
+        />
+        <button className="form-btn">{register ? "Sign Up" : "Login"}</button>
+      </form>
+      <button className="form-btn">
+        Need to {register ? "Login" : "Sign Up"}?
+      </button>
+    </main>
+  );
+};
+
+export default Auth;
